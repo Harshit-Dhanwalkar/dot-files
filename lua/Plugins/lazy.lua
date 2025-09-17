7eedf00a31626be846e81b8c0163aa696d6f611c
Harshit-Dhanwalkar <harshitpd1729@gmail.com>
2025-09-17 23:51:03 +0530

    Customise statusline

--- lua/Plugins/lazy.lua
+++ lua/Plugins/lazy.lua
@@ -684,29 +684,18 @@ require("lazy").setup({
 	{ -- Collection of various small independent plugins/modules
 		"echasnovski/mini.nvim",
 		config = function()
-			-- Better Around/Inside textobjects
-			-- Examples:
-			--  - va)  - [V]isually select [A]round [)]paren
-			--  - yinb - [Y]ank [I]nside [N]ext [B]rackets
-			--  - yin' - [Y]ank [I]nside [N]ext [Q]uote
-			--  - ci'  - [C]hange [I]nside [']quote
-			--  - cib  - [C]hange [I]nside [B]rackets
 			require("mini.ai").setup({ n_lines = 500 })
 			-- Add/delete/replace surroundings (brackets, quotes, etc.)
 			-- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
 			-- - sd'   - [S]urround [D]elete [']quotes
 			-- - sr)'  - [S]urround [R]eplace [)] [']
 			require("mini.surround").setup()
-			-- Simple and easy statusline.
-			-- TODO: modify statusline
-			local statusline = require("mini.statusline")
-			statusline.setup({ use_icons = vim.g.have_nerd_font })
-			-- You can configure sections in the statusline by overriding their default behavior. For example, here we set the section for cursor location to LINE:COLUMN
-			---@diagnostic disable-next-line: duplicate-set-field
-			statusline.section_location = function()
-				return "%2l:%-2v"
-			end
-			--  Check out: https://github.com/echasnovski/mini.nvim
+			-- local statusline = require("mini.statusline")
+			-- statusline.setup({ use_icons = vim.g.have_nerd_font })
+			-- ---@diagnostic disable-next-line: duplicate-set-field
+			-- statusline.section_location = function()
+			-- 	return "%2l:%-2v"
+			-- end
 		end,
 	},
 	{
@@ -1065,6 +1054,7 @@ require("lazy").setup({
 	--{
 	-- 'mileszs/ack.vim'
 	-- }
+	require("Plugins.statusline"),
 	require("Plugins.debug"),
 	require("Plugins.indent_line"),
 	require("Plugins.lint"),
---
+++ lua/Plugins/statusline.lua
@@ -0,0 +1,136 @@
+local function get_time()
+	return os.date("%I:%M %p")
+end
+
+return {
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "echasnovski/mini.icons" },
+	config = function()
+		require("lualine").setup({
+			options = {
+				icons_enabled = true,
+				theme = "auto",
+				component_separators = { left = "", right = "" },
+				section_separators = { left = "", right = "" },
+				disabled_filetypes = {
+					statusline = {},
+					winbar = {},
+				},
+				ignore_focus = {},
+				always_divide_middle = true,
+				always_show_tabline = true,
+				globalstatus = false,
+				refresh = {
+					statusline = 1000,
+					tabline = 1000,
+					winbar = 1000,
+					refresh_time = 16, -- ~60fps
+					events = {
+						"WinEnter",
+						"BufEnter",
+						"BufWritePost",
+						"SessionLoadPost",
+						"FileChangedShellPost",
+						"VimResized",
+						"Filetype",
+						"CursorMoved",
+						"CursorMovedI",
+						"ModeChanged",
+					},
+				},
+			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch", "diff", "diagnostics" },
+				lualine_c = {
+					{
+						"filename",
+						file_status = true,
+						newfile_status = false,
+						path = 0,
+						-- 1: Relative path
+						-- 2: Absolute path
+						-- 3: Absolute path, with tilde as the home directory
+						-- 4: Filename and parent dir, with tilde as the home directory
+						shorting_target = 40,
+						symbols = {
+							modified = "[+]",
+							readonly = "[-]",
+							unnamed = "[No Name]",
+							newfile = "[New]",
+						},
+					},
+					"diagnostics",
+				},
+				lualine_x = {
+					-- {
+					-- 	"encoding",
+					-- 	-- Show '[BOM]' when the file has a byte-order mark
+					-- 	show_bomb = false,
+					-- },
+					{
+						"fileformat",
+						symbols = {
+							unix = "", -- e712
+							dos = "", -- e70f
+							mac = "", -- e711
+						},
+					},
+					{
+						"filetype",
+						colored = true, -- Displays filetype icon in color if set to true
+						icon_only = false, -- Display only an icon for filetype
+						icon = { align = "right" },
+					},
+				},
+				-- lualine_y = { "progress" },
+				lualine_y = {
+					{
+						"lsp_status",
+						icon = "", -- f013
+						symbols = {
+							spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" },
+							done = "✓",
+							separator = " ",
+						},
+						ignore_lsp = {},
+					},
+				},
+				lualine_z = { get_time, "location" },
+			},
+			inactive_sections = {
+				lualine_a = {},
+				lualine_b = {},
+				lualine_c = { "filename" },
+				lualine_x = { "location" },
+				lualine_y = {},
+				lualine_z = {},
+			},
+			-- tabline = {
+			-- 	lualine_a = {},
+			-- 	lualine_b = { "branch" },
+			-- 	lualine_c = { "filename" },
+			-- 	lualine_x = {},
+			-- 	lualine_y = {},
+			-- 	lualine_z = {"tabs"},
+			-- },
+			-- winbar = {
+			-- 	lualine_a = {},
+			-- 	lualine_b = {},
+			-- 	lualine_c = { "filename" },
+			-- 	lualine_x = {},
+			-- 	lualine_y = {},
+			-- 	lualine_z = {},
+			-- },
+			-- inactive_winbar = {
+			-- 	lualine_a = {},
+			-- 	lualine_b = {},
+			-- 	lualine_c = { "filename" },
+			-- 	lualine_x = {},
+			-- 	lualine_y = {},
+			-- 	lualine_z = {},
+			-- },
+			extensions = {},
+		})
+	end,
+}
