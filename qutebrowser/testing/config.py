90bc3bf42283721b2d5a3bdc2652c4ee7e8ce8fe
Harshit-Dhanwalkar <harshitpd1729@gmail.com>
2026-05-02 18:40:08 +0530

    Add zathura, btop themes. Update waybar, qutebrower config

---
+++ qutebrowser/testing/config.py
@@ -0,0 +1,45 @@
+# Remove LSP warnings
+from qutebrowser.config.config import ConfigContainer
+from qutebrowser.config.configfiles import ConfigAPI
+
+config: ConfigAPI = config
+c: ConfigContainer = c
+# from qutebrowser.api import interceptor
+
+# Disable loading autoconfig
+config.load_autoconfig(False)
+
+# Autosave session when qutebrowser is being closed
+c.auto_save.session = True
+
+# Darkmode settings
+# c.colors.webpage.darkmode.enabled = True
+c.colors.webpage.preferred_color_scheme = "dark"
+# c.colors.webpage.darkmode.policy.images = "never"
+# c.colors.webpage.darkmode.threshold.background = 100
+
+# Disable autoplay
+c.content.autoplay = False
+
+# Source theme config file
+config.source("gruvbox.py")
+
+# qute-pass bindings
+basecmd = 'spawn --userscript qute-pass -M gopass -d "bemenu -p pass"'
+config.bind("<z><l>", basecmd)
+config.bind("<z><u><l>", basecmd + " --username-only")
+config.bind("<z><p><l>", basecmd + " --password-only")
+config.bind("<z><o><l>", basecmd + " --otp-only")
+
+# Font
+c.fonts.default_family = "JetBrains Mono"
+c.fonts.default_size = "10pt"
+
+# Yes, I just want to find what I need when I need to, don't blame me
+c.url.default_page = "https://www.google.com/"
+c.url.start_pages = c.url.default_page
+c.url.searchengines = {
+    "DEFAULT": "https://www.google.com/search?q={}",
+    "!repology": "https://repology.org/projects/?search={}",
+    "!reversocontext": "https://context.reverso.net/translation/english-russian/{}",
+}
--- wallpapers/README.md
+++ wallpapers/README.md
@@ -1,3 +1,3 @@
 # Wallpaper switcher script
 
-[layer](https://github.com/Harshit-Dhanwalkar/layer.git) is own implementation of a wallpaper switcher in C, works with `swaybg` and `feh` to set wallpapers and preview them natively using default builtin `imageviewer` or any other image viewer like `sxiv` or `viu`.
+[layer](https://github.com/Harshit-Dhanwalkar/layer.git) is my own implementation of a wallpaper switcher in C, works with `swaybg` and `feh` to set wallpapers and preview them natively using default builtin `imageviewer` or any other image viewer like `sxiv` or `viu`.
